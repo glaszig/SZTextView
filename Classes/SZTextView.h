91edd576aded970c92440cad8aff72e7caa1cede
glaszig <glaszig@gmail.com>
2013-03-14 06:13:47 +0100

    moving files around

---
+++ Classes/SZTextView.h
@@ -0,0 +1,13 @@
+//
+//  SZTextView.h
+//  SZTextView
+//
+//  Created by glaszig on 14.03.13.
+//  Copyright (c) 2013 glaszig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SZTextView : UITextView
+
+@end
---
+++ Classes/SZTextView.m
@@ -0,0 +1,31 @@
+//
+//  SZTextView.m
+//  SZTextView
+//
+//  Created by glaszig on 14.03.13.
+//  Copyright (c) 2013 glaszig. All rights reserved.
+//
+
+#import "SZTextView.h"
+
+@implementation SZTextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
