ceebb41de93fbcf7f32f8682a651022eaddbd3b5
Matt Smollinger <m.smollinger@gmail.com>
2021-08-21 12:34:46 -0400

    Expose headers publicly

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SZTextView",
-            path: "Classes")
+            path: "Classes",
+            publicHeadersPath: "Classes")
     ]
 )
