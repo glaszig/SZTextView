c632ababbc5a27e48c08270ca6440a1c0fe553f3
Matt Smollinger <m.smollinger@gmail.com>
2021-08-22 14:31:37 -0400

    Refactor directory structure a smidge - breaks pods but easy fix soon

--- Package.swift
+++ Package.swift
@@ -6,18 +6,15 @@ import PackageDescription
 let package = Package(
     name: "SZTextView",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SZTextView",
             targets: ["SZTextView"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SZTextView",
-            path: "Classes",
-            sources: ["Classes"],
-            publicHeadersPath: "Classes")
+            path: "SZTextView",
+            sources: ["Sources"],
+            publicHeadersPath: "Sources")
     ]
 )
