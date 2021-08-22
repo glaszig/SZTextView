f6ee65b50f7100318561b1ade4e17e66a3755e4e
Matt Smollinger <m.smollinger@gmail.com>
2021-08-22 14:42:03 -0400

    Exclude Info.plist to solve warning

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .target(
             name: "SZTextView",
             path: "SZTextView",
+            exclude: ["Info.plist"],
             sources: ["Sources"],
             publicHeadersPath: "Sources")
     ]
